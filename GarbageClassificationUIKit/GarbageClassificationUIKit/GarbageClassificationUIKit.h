8ddb554bf3015c3e29121574429750787f1efe6f
wpf <1569485690@qq.com>
2019-07-16 10:32:47 +0800

    create project

---
+++ GarbageClassificationUIKit/GarbageClassificationUIKit/GarbageClassificationUIKit.h
@@ -0,0 +1,19 @@
+//
+//  GarbageClassificationUIKit.h
+//  GarbageClassificationUIKit
+//
+//  Created by YB on 2019/7/16.
+//  Copyright © 2019 Mr.wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GarbageClassificationUIKit.
+FOUNDATION_EXPORT double GarbageClassificationUIKitVersionNumber;
+
+//! Project version string for GarbageClassificationUIKit.
+FOUNDATION_EXPORT const unsigned char GarbageClassificationUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GarbageClassificationUIKit/PublicHeader.h>
+
+
