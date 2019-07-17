2ce3a7a5456bb920993c1553fbd90cc8519a9139
wpf <1569485690@qq.com>
2019-07-17 14:11:08 +0800

    Create SearchViewController

---
+++ Sources/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  GarbageClassificationUIKit
+//
+//  Created by YB on 2019/7/17.
+//  Copyright © 2019 Mr.wu. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
